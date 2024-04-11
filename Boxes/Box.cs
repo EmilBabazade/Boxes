982952865306caada61612dad7ea1076ed61839e
Emil Babazade <emil.babazade@atea.com>
2024-04-11 22:03:21 +0300

    Add project files.

---
+++ Boxes/Box.cs
@@ -0,0 +1,33 @@
+﻿namespace Boxes;
+internal class Box
+{
+    public required string SupplierId { get; set; }
+    public required string Id { get; set; }
+
+    public required ICollection<Item> Items { get; set; }
+
+    // just to make logging easier
+    public override string ToString()
+    {
+        var str = $"Box\tId: {Id}\tSupplierId: {SupplierId}\tItems:";
+        foreach(var item in Items )
+        {
+            str += "\n" + item;
+        }
+        return str;
+    }
+}
+
+internal class Item
+{
+    public required long ISBN { get; set; }
+    public required string PO { get; set; }
+
+    public required int Qty { get; set; }
+
+    // just to make logging easier
+    public override string ToString()
+    {
+        return $"\tItem\tISBN: {ISBN}\tPO {PO}\tQuantity {Qty}";
+    }
+}
---
+++ Boxes/Program.cs
@@ -0,0 +1,83 @@
+﻿using Boxes;
+using System.Text;
+
+// TODO: scan dir for files
+
+// TODO: read file in chunks
+using var fs = new FileStream("data.txt", FileMode.Open);
+var buffer = new byte[1024];
+var boxes = new List<Box>();
+Box box = null;
+List<string> cutUpLine = null;
+while(await fs.ReadAsync(buffer, 0, buffer.Length) > 0)
+{
+    // TODO: from string to box objects
+    var data = Encoding.Default.GetString(buffer);
+    var lines = data.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    foreach (var line in lines)
+    {
+        var elements = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        // TODO: VALIDATE TEXT!!!!!!
+
+        // TODO: HOW TO HANDLE CUT UP LINES?
+
+
+        if (elements[0] == "HDR" && elements.Count() == 3)
+        {
+            box = new Box
+            {
+                SupplierId = elements[1],
+                Id = elements[2],
+                Items = new List<Item>()
+            };
+            boxes.Add(box);
+        }
+        else if (elements[0] == "LINE" && elements.Count() == 4)
+        {
+            var item = new Item
+            {
+                PO = elements[1],
+                ISBN = long.Parse(elements[2]), // TODO: Error invalid PO -> log and continue
+                Qty = int.Parse(elements[3]) // TODO: Error invalid Quantity -> log and continue
+            };
+            box.Items.Add(item);
+        }
+        else if (cutUpLine == null) // handle cut up line
+        {
+            cutUpLine = [.. elements];
+        }
+        else
+        {
+            cutUpLine.AddRange(elements);
+            if (cutUpLine[0] == "HDR")
+            {
+                box = new Box
+                {
+                    SupplierId = cutUpLine[1],
+                    Id = cutUpLine[2],
+                    Items = new List<Item>()
+                };
+                boxes.Add(box);
+            }
+            else if (elements[0] == "LINE")
+            {
+                var item = new Item
+                {
+                    PO = cutUpLine[1],
+                    ISBN = long.Parse(cutUpLine[2]), // TODO: Error invalid PO -> log and continue
+                    Qty = int.Parse(cutUpLine[3]) // TODO: Error invalid Quantity -> log and continue
+                };
+                box.Items.Add(item);
+            }
+            cutUpLine = null;
+        }
+
+        // TODO: insert box to db
+    }
+    // TODO: insert/update db
+}
+
+// TODO: delete file or move to processed directory
+
+foreach (var b in boxes)
+    Console.WriteLine(b);
